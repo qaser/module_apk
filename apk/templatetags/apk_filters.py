bd36ed89b414931b5a9a6853d61105eaa7960d9c
qaser <huji@mail.ru>
2021-09-26 21:30:02 +0500

    add login form styles

--- apk/templatetags/apk_filters.py
+++ apk/templatetags/apk_filters.py
@@ -3,9 +3,9 @@ from django import template
 register = template.Library()
 
 
-# @register.filter
-# def addclass(field, css):
-#     return field.as_widget(attrs={'class': css})
+@register.filter
+def addclass(field, css):
+    return field.as_widget(attrs={'class': css})
 
 
 # склонение слов
