17e1fdd60b28dbf900daa63c4f0cf1398571517c
qaser <huji@mail.ru>
2021-10-02 19:56:54 +0500

    add 1 level apk

---
+++ apk/filters.py
@@ -0,0 +1,17 @@
+# from apk.forms import FaultFilterForm
+import django_filters as df
+from .models import Department, Fault, Location
+
+
+class FaultFilter(df.FilterSet):
+    # department = df.ModelChoiceFilter(field_name='Служба филиала', queryset=Department.objects.all())
+    # location = df.ModelChoiceFilter(field_name='Место обнаружения', queryset=Location.objects.filter(department=department))
+
+    class Meta:
+        model = Fault
+        fields = [
+            'fault_date',
+            'location',
+            # 'inspector' 
+        ]
+        # form = FaultFilterForm
